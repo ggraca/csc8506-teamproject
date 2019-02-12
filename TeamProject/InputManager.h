c7aea067afc196ca8ace6c0b75ad836edc30e389
Berkem Cinar <cinar.berkem@gmail.com>
2019-02-12 13:19:38 +0000

    Initial Implementation

---
+++ TeamProject/InputManager.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "ExampleScene.h"
+#include <vector>
+
+using namespace NCL;
+using namespace CSC8503;
+
+class InputManager
+{
+public:
+	static enum ActionButton
+	{
+		FORWARD,
+		BACKWARD,
+		LEFT,
+		RIGHT,
+		FIRE,
+		MAX
+	};
+
+	InputManager();
+	~InputManager();
+
+	
+	bool IsButtonPressed(ActionButton actionButton);
+	bool IsButtonHeld(ActionButton actionButton);
+	bool IsButtonDown(ActionButton actionButton);
+	bool IsDoubleClicked(ActionButton actionButton);
+
+private:
+
+	struct ButtonRelation
+	{
+		std::vector<NCL::KeyboardKeys> relatedKeyboardKeys;
+		std::vector<NCL::MouseButtons> relatedMouseKeys;
+	};
+
+	void InitializeButtonRelations();
+
+protected:
+
+	ButtonRelation * registeredActionButtons;
+
+};
+
