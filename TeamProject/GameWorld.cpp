d8dd7b5e4e9dd43e61244faadc834ff0700b7cc1
sbendell <43750640+sbendell@users.noreply.github.com>
2019-03-05 20:50:04 +0000

    Fixed creating gameobject without physics object

--- TeamProject/GameWorld.cpp
+++ TeamProject/GameWorld.cpp
@@ -146,11 +146,15 @@ void GameWorld::AddGameObject(GameObject* o)
 	CallInitialObjectFunctions(o);
 	gameObjects.push_back(o);
 
-	btCollisionShape* po = o->GetComponent<PhysicsObject*>()->GetShape();
-	physics->collisionShapes.push_back(po);
+	PhysicsObject* physicsO = o->GetComponent<PhysicsObject*>();
 
-	btRigidBody* pb = o->GetComponent<PhysicsObject*>()->GetRigidbody();
-	physics->dynamicsWorld->addRigidBody(pb);
+	if (physicsO) {
+		btCollisionShape* po = o->GetComponent<PhysicsObject*>()->GetShape();
+		physics->collisionShapes.push_back(po);
+
+		btRigidBody* pb = o->GetComponent<PhysicsObject*>()->GetRigidbody();
+		physics->dynamicsWorld->addRigidBody(pb);
+	}
 }
 
 void GameWorld::CallInitialObjectFunctions(NCL::CSC8503::GameObject * o)
