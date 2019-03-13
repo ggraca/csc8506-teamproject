74ddd1fc9b070c410649950a5fa27e80a4c70efd
sbendell <43750640+sbendell@users.noreply.github.com>
2019-03-13 15:09:33 +0000

    Implemented constructor

--- TeamProject/ParticleSystem.cpp
+++ TeamProject/ParticleSystem.cpp
@@ -3,6 +3,21 @@
 using namespace NCL;
 using namespace CSC8503;
 
+ParticleSystem::ParticleSystem(float Duration, int MaxParticles, int ParticleSpawnRate, float ParticleLifetime,
+	float ParticleStartSpeed, Vector3 ParticleSpeedDirection, Vector3 ParticleSpeedDeviation,
+	float ParticleStartSize) {
+
+	duration = Duration;
+	maxParticles = MaxParticles;
+	particleSpawnRate = ParticleSpawnRate;
+
+	particleLifetime = ParticleLifetime;
+	particleStartSpeed = ParticleStartSpeed;
+	particleSpeedDirection = ParticleSpeedDirection;
+	particleSpeedDeviation = ParticleSpeedDeviation;
+	particleStartSize = ParticleStartSize;
+}
+
 ParticleSystem::ParticleSystem()
 {
 }
