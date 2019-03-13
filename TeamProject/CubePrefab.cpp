0f2bd7e980f09f8ab60d285990d51032fcee006a
Berkem Cinar <cinar.berkem@gmail.com>
2019-03-13 12:03:25 +0000

    Small refactoring

--- TeamProject/CubePrefab.cpp
+++ TeamProject/CubePrefab.cpp
@@ -3,9 +3,7 @@
 
 CubePrefab::CubePrefab()
 {
-	GetTransform().SetWorldScale(Vector3(1, 1, 1));
-	GetTransform().SetWorldPosition(Vector3(0, 0, 0));
-	GetTransform().SetLocalOrientation(Quaternion::EulerAnglesToQuaternion(0, 0, 0));
+	SetTransformDetails(Vector3(1,1,1), Vector3(0, 0, 0), Quaternion::EulerAnglesToQuaternion(0, 0, 0));
 	AddComponent<PhysicsObject*>((Component *)new PhysicsObject(&GetTransform(), ShapeType::cube, 10));
 	AddComponent<RenderObject*>(new RenderObject(&GetTransform(), Assets::AssetManager::LoadMesh("Cube.msh"), Assets::AssetManager::LoadMaterial("Basic Material", Assets::AssetManager::LoadShader("basicShader","pbrvert.glsl", "pbrfrag.glsl"))));
 	GetComponent<RenderObject*>()->SetMaterialInstanced();
@@ -13,9 +11,7 @@ CubePrefab::CubePrefab()
 
 CubePrefab::CubePrefab(const Vector3& position, const Quaternion& orient, Vector3 dimensions, float mass, float restitution, float friction)
 {
-	GetTransform().SetWorldScale(dimensions);
-	GetTransform().SetWorldPosition(position);
-	GetTransform().SetLocalOrientation(orient);
+	SetTransformDetails(dimensions, position, orient);
 	AddComponent<PhysicsObject*>((Component *)new PhysicsObject(&GetTransform(), ShapeType::cube, mass, restitution, friction));
 	AddComponent<RenderObject*>(new RenderObject(&GetTransform(), Assets::AssetManager::LoadMesh("Cube.msh"), Assets::AssetManager::LoadMaterial("Basic Material", Assets::AssetManager::LoadShader("PBRShader", "pbrvert.glsl", "pbrfrag.glsl"))));
 	GetComponent<RenderObject*>()->SetMaterialInstanced();
@@ -26,13 +22,18 @@ CubePrefab::CubePrefab(CubePrefab::PrefabType type)
 	switch (type)
 	{
 	case CubePrefab::GUN: // I didn't want to create a prefab class just for these lines.
-		GetTransform().SetWorldScale(Vector3(0.4, 0.4, 2.5));
-		GetTransform().SetWorldPosition(Vector3(0, 0, 0));
-		GetTransform().SetLocalOrientation(Quaternion::EulerAnglesToQuaternion(0, 0, 0));
+		SetTransformDetails(Vector3(0.4, 0.4, 2.5), Vector3(0, 0, 0), Quaternion::EulerAnglesToQuaternion(0, 0, 0));
 		AddComponent<RenderObject*>(new RenderObject(&GetTransform(), Assets::AssetManager::LoadMesh("Cube.msh"), Assets::AssetManager::LoadMaterial("Basic Material", Assets::AssetManager::LoadShader("basicShader", "pbrvert.glsl", "pbrfrag.glsl"))));
 		GetComponent<RenderObject*>()->SetMaterialInstanced();
 		return;
 	}
+}
+
+void CubePrefab::SetTransformDetails(NCL::Maths::Vector3 &dimensions, const NCL::Maths::Vector3 & position, const NCL::Maths::Quaternion & orient)
+{
+	GetTransform().SetWorldScale(dimensions);
+	GetTransform().SetWorldPosition(position);
+	GetTransform().SetLocalOrientation(orient);
 }
 
 CubePrefab::~CubePrefab()
