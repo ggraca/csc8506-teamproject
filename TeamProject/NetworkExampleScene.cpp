3adfac1025b6e04c4d0a83386ddbff5f345ed6a4
Ed <peinchka@hotmail.com>
2019-03-26 17:46:31 +0000

    Complex meshes working again!

--- TeamProject/NetworkExampleScene.cpp
+++ TeamProject/NetworkExampleScene.cpp
@@ -49,14 +49,14 @@ void NetworkExampleScene::ResetWorld() {
 		}
 	}*/
 //	auto ball = new SpherePrefab(Vector3(1.6, 30, 1.6), 30, 0, 0.5, 0.5);
-//	auto cone = new ConePrefab(Vector3(1.6, 10, 1.6), Quaternion::AxisAngleToQuaternion(Vector3(0, 0, 0), 0), Vector3(160, 100, 160), 0, 0.5, 0.5);
-
+	auto cone = new ConePrefab(Vector3(0, 0, 0), Quaternion::AxisAngleToQuaternion(Vector3(0, 0, 0), 0), Vector3(1, 1, 1), 0, 0.5, 0.5);
+	cone->SetTag(LayerAndTag::Tags::Ground);
 	//auto well = new WellPrefab(Vector3(50, 50, 50), Vector3(-100, 20, -100), Quaternion::AxisAngleToQuaternion(Vector3(0, 1, 0), 90));
 
 	world->LateInstantiateRecursively(player);
 	world->LateInstantiate(floor);
 //	world->LateInstantiate(ball);
-//	world->LateInstantiateRecursively(cone);
+	world->LateInstantiateRecursively(cone);
 
 	world->GetMainCamera()->GetComponent<CameraControl*>()->SetPlayer(player);
 }
