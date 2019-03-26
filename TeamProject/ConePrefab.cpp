6f661ffece3000634a12c1009d128be227033559
Ed <peinchka@hotmail.com>
2019-03-26 08:46:18 +0000

    Cone Prefab

--- TeamProject/ConePrefab.cpp
+++ TeamProject/ConePrefab.cpp
@@ -9,11 +9,14 @@ ConePrefab::ConePrefab()
 	GetComponent<RenderObject*>()->SetMaterialInstanced();
 }
 
-ConePrefab::ConePrefab(const Vector3& position, float radius, float mass, float restitution, float friction)
+ConePrefab::ConePrefab(const Vector3& position, const Quaternion& orient, Vector3 dimensions, float mass, float restitution, float friction)
 {
-	GetTransform().SetWorldScale(Vector3(radius, radius, radius));
+	GameObject* cone = GameObject::FromOBJ(coneMesh);
+
+	GetTransform().SetWorldScale(dimensions);
 	GetTransform().SetWorldPosition(position);
-	AddComponent<PhysicsObject*>((Component *)new PhysicsObject(&GetTransform(), ShapeType::cone, mass, restitution, friction));
+	GetTransform().SetLocalOrientation(orient);
+	AddComponent<PhysicsObject*>((Component*)new PhysicsObject(&cone->GetTransform(), ShapeType::cone, mass, restitution, friction));
 	AddComponent<RenderObject*>(new RenderObject(&GetTransform(), Assets::AssetManager::LoadMesh("Cone.msh"), Assets::AssetManager::LoadMaterial("Basic Material", Assets::AssetManager::LoadShader("basicShader", "pbrvert.glsl", "pbrfrag.glsl"))));
 	GetComponent<RenderObject*>()->SetMaterialInstanced();
 }
